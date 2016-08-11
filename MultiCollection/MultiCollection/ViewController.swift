f92d1f44b592585b86c7bda584bfbe29a0ca3efe
Eric Ho <ericho@101MediaLabs-iMac-Ho-Ho.local>
2016-08-11 17:47:21 +0800

    no message

---
+++ MultiCollection/MultiCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiCollection
+//
+//  Created by EricHo on 11/8/2016.
+//  Copyright © 2016 E H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
